dc229aae5cc672ccd91e6be85c02a86bee0f572f
Dilan Tompkins <65461019+TomTerrific7@users.noreply.github.com>
2021-08-21 23:42:53 -0400

    Updated contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -10,26 +10,24 @@ contract Lottery is VRFConsumerBase {
 
   enum LotteryState {Open, Closed, Complete}
    
-   mapping(address => bool) public eligiblePlayer;
+   
    LotteryState public state;
-   address payable winner;
    address public owner;
    bytes32 internal keyHash;
    uint internal fee;
    uint public randomResult;
    uint public lotteryID;
+   address [] public players;
     
   
    //events
-  event PaidWinner(address from, address _winner);
+  event PaidWinner(address from, address winner);
   event newPlayer(address _player);
 
   modifier isState(LotteryState _state) {
 		require(state == _state, "Wrong state");
 		_;
-  }
-  
-  
+  } 
   constructor () VRFConsumerBase(
             0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator rinkeby
             0x01BE23585060835E02B77ef475b0Cc51aA1e0709  // LINK Token rinkeby
@@ -40,45 +38,39 @@ contract Lottery is VRFConsumerBase {
         lotteryID = 1;
     }
 
-    function getRansomNumber() public returns (bytes32 requestId){
-      require(LINK.balanceOf(address(this)) >= fee,"Need more LINK");
-      return requestRandomness(keyHash, fee); 
-      
+  function getRandomNumber() public returns (bytes32 requestId){
+    require(LINK.balanceOf(address(this)) >= fee,"Need more LINK");
+    return requestRandomness(keyHash, fee);
+    
     }
   
  function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {  
-    randomResult = (randomness % 100) + 1;
-    
-}
+    randomResult = (randomness % players.length) + 1;
+    uint winner = randomResult;
+    payable(players[winner]).transfer(address(this).balance);
+    emit PaidWinner(address(this), players[winner]);
+
+    }
   function startNewLottery(uint256 duration) public {
     require(state == LotteryState.Closed);
     state = LotteryState.Open;
+    duration = 3 days;
     lotteryID += 1;
-    
-  }
 
-   function enterLottery(address) external payable isState(LotteryState.Open) {
-     require (msg.value == 1); 
-     eligiblePlayer[msg.sender] = true;
+    }
+
+   function enterLottery() external payable isState(LotteryState.Open) {
+     require (msg.value == .01 ether); 
+     players.push(msg.sender);
      emit newPlayer(msg.sender);
 
-      }
+    }
+   function getBalance() public view returns (uint) {
+     return address(this).balance;
 
-
-       function payWinner(address payable _winner) public {
-           winner = _winner;
-         winner.transfer(address(this).balance);
-         emit PaidWinner(msg.sender, _winner);
-         
-       }
-       
-       function getBalance() public view returns (uint) {
-        return address(this).balance;
-        
-}
-
+    }
         receive() external payable {
-    
-}
+
+        }
 
 }
