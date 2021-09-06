651312de2622c0be14d74f9ceddf2aa20a7b02c3
Dilan Tompkins <65461019+TomTerrific7@users.noreply.github.com>
2021-09-06 17:35:08 -0400

    added walletConnect and Formatic

--- front-end/providerOptions.js
+++ front-end/providerOptions.js
@@ -1,10 +1,23 @@
 import Portis from "@portis/web3";
-
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Fortmatic from "fortmatic";
 const providerOptions = {
   portis: {
     package: Portis, 
     options: {
       id: "4ae59703-f6d5-49cd-a727-5145998c55cd"
+    }
+  },
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: "INFURA_ID" // required
+    }
+  },
+  fortmatic: {
+    package: Fortmatic, // required
+    options: {
+      key: "FORTMATIC_KEY" // required
     }
   }
 };
