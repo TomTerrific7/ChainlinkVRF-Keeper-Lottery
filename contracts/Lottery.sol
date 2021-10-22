e848969e3ef0c1c71a0ef25a5232c29924372472
Dilan Tompkins <65461019+TomTerrific7@users.noreply.github.com>
2021-10-22 01:17:28 -0400

    Chainlink keepers $ VRF working together now

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -19,20 +19,22 @@ contract Lottery is VRFConsumerBase, KeeperCompatibleInterface {
    uint  public lotteryID;
    address [] public players;
    uint public winner;
-  uint public immutable interval;
   uint public previousID;
   uint public lastLottery;
+  uint startTime;
    //events
   event PaidWinner(address from, address winner);
   event newPlayer(address _player);
   event RequestNumber(bytes32 indexed requestId);
-  event RequestFulFilled(bytes32 indexed requestId, uint256 indexed result);
-  event NewUpkeep(bytes indexed performData);
-  
+ 
+ 
+
   modifier isState(LotteryState _state) {
 		require(state == _state, "Wrong state");
 		_;
   } 
+
+
   constructor () VRFConsumerBase(
     0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator rinkeby
     0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token rinkeby
@@ -43,6 +45,7 @@ contract Lottery is VRFConsumerBase, KeeperCompatibleInterface {
         owner = msg.sender;
        lotteryID = 1;
        previousID = 0;
+       startTime = block.timestamp;
 
         
     }
@@ -51,14 +54,17 @@ contract Lottery is VRFConsumerBase, KeeperCompatibleInterface {
    
     function checkUpkeep(bytes calldata checkData) external override returns (bool upkeepNeeded, bytes memory performData)  {
       
-      upkeepNeeded = (lotteryID - 1) == previousID;
-        performData = checkData;
-        emit NewUpkeep(performData);
+      upkeepNeeded = (lotteryID - 1) == previousID || (block.timestamp >= startTime + 240 seconds && state == LotteryState.Open);
+      performData = checkData;
   }
 
   function performUpkeep(bytes calldata /* performData */) external override {
-      startNewLottery();  
-      duration();
+      if(state == LotteryState.Closed) {
+          startNewLottery();
+      } else if(state == LotteryState.Open) {
+          getRandomNumber();
+      }
+      
           
       
   }  
@@ -77,22 +83,25 @@ contract Lottery is VRFConsumerBase, KeeperCompatibleInterface {
      state = LotteryState.Calculating;
     randomResult = (randomness % players.length);
     winner = randomResult;
-    emit RequestFulFilled(requestId, randomResult);
+    payable(players[winner]).transfer(address(this).balance);
+      emit PaidWinner(address(this), players[winner]);
+      delete players;
+      state= LotteryState.Closed;
+      previousID +=1;
+    
+  
     }
     
-    function duration() public {
-        require(state == LotteryState.Open);
-        require(block.timestamp - lastLottery > 30 seconds, 'Need to wait 5 minutes');
-        getRandomNumber();
-        lastLottery = block.timestamp;
-    }
+   
     
-    function startNewLottery() public {
+    function startNewLottery() public  {
       require(state == LotteryState.Closed);
       state = LotteryState.Open;
       lotteryID += 1;
       
+      
     }
+
     
     function enterLottery() external payable isState(LotteryState.Open) {
       require (msg.value == 1 wei); 
@@ -100,14 +109,7 @@ contract Lottery is VRFConsumerBase, KeeperCompatibleInterface {
       emit newPlayer(msg.sender);
  
     }
-    function payWinner() public {
-      require(state == LotteryState.Calculating);
-      payable(players[winner]).transfer(address(this).balance);
-      emit PaidWinner(address(this), players[winner]);
-      delete players;
-      state= LotteryState.Closed;
-      previousID +=1;
-}
+   
 
    function getBalance() public view returns (uint) {
      return address(this).balance;
@@ -116,3 +118,4 @@ contract Lottery is VRFConsumerBase, KeeperCompatibleInterface {
         receive() external payable {
 
         }
+      }
